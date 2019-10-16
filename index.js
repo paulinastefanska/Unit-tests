7d591eadc160358706f991461da00e5c2a739326
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-16 11:00:49 +0200

    Add format time

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
+
+	if (!timeInSeconds) {
+		return `0s`;
+	}
+
+	let seconds = timeInSeconds;
+	let minutes = timeInSeconds > 60 ? Math.floor(timeInSeconds / 60) : 0;
+	let hours = timeInSeconds > 3600 ? Math.floor(timeInSeconds / 3600) : 0;
+
+	minutes = minutes - hours * 60;
+	seconds = seconds - minutes * 60 - hours * 3600;
+
+	const result = (hours > 0 ? `${hours}h` : ``) + (minutes > 0 ? `${minutes}m` : ``) + (seconds > 0 ? `${seconds}s` : ``)
+
+  return result.substring(0, result.lenght - 1);
 }
 
 module.exports = formatDate;
